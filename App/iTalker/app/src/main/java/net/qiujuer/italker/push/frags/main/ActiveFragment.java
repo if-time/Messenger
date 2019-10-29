67cee4dfcdc1ff2fd8a4b97fe3f19d63ac425c44
Dong <690443961@qq.com>
2019-10-29 17:16:17 +0800

    Update ActiveFragment.java

--- App/iTalker/app/src/main/java/net/qiujuer/italker/push/frags/main/ActiveFragment.java
+++ App/iTalker/app/src/main/java/net/qiujuer/italker/push/frags/main/ActiveFragment.java
@@ -2,11 +2,11 @@ package net.qiujuer.italker.push.frags.main;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import net.qiujuer.italker.common.app.Fragment;
 import net.qiujuer.italker.push.R;
 
 /**
@@ -19,11 +19,9 @@ public class ActiveFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_active, container, false);
+    protected int getContentLayoutId() {
+        return R.layout.fragment_active;
     }
 
 }
