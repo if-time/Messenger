9175db52d4c6e51d65744151596294feae8f5e99
Dong <690443961@qq.com>
2019-11-04 09:28:40 +0800

    主界面底部导航栏封装

--- App/iTalker/app/src/main/java/net/qiujuer/italker/push/MainActivity.java
+++ App/iTalker/app/src/main/java/net/qiujuer/italker/push/MainActivity.java
@@ -2,8 +2,10 @@ package net.qiujuer.italker.push;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -12,8 +14,16 @@ import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.ViewTarget;
 
+import net.qiujuer.genius.ui.Ui;
+import net.qiujuer.genius.ui.widget.FloatActionButton;
 import net.qiujuer.italker.common.app.Activity;
 import net.qiujuer.italker.common.widget.PortraitView;
+import net.qiujuer.italker.push.frags.main.ActiveFragment;
+import net.qiujuer.italker.push.frags.main.ContactFragment;
+import net.qiujuer.italker.push.frags.main.GroupFragment;
+import net.qiujuer.italker.push.helper.NavHelper;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -21,7 +31,7 @@ import butterknife.OnClick;
 /**
  * @author dong
  */
-public class MainActivity extends Activity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends Activity implements BottomNavigationView.OnNavigationItemSelectedListener, NavHelper.OnTabChangedListener<Integer> {
 
 
     @BindView(R.id.appbar)
@@ -39,6 +49,11 @@ public class MainActivity extends Activity implements BottomNavigationView.OnNav
     @BindView(R.id.navigation)
     BottomNavigationView mNavigation;
 
+    @BindView(R.id.btn_action)
+    FloatActionButton mAction;
+
+    private NavHelper<Integer> navHelper;
+
 
     @Override
     protected int getContentLayoutId() {
@@ -49,9 +64,19 @@ public class MainActivity extends Activity implements BottomNavigationView.OnNav
     protected void initWidget() {
         super.initWidget();
 
+        // 初始化工具类
+        navHelper = new NavHelper<>(this, R.id.lay_container, getSupportFragmentManager(), this);
+        navHelper.add(R.id.action_home, new NavHelper.Tab<Integer>(ActiveFragment.class, R.string.title_home))
+                .add(R.id.action_group, new NavHelper.Tab<Integer>(GroupFragment.class, R.string.title_group))
+                .add(R.id.action_contact, new NavHelper.Tab<Integer>(ContactFragment.class, R.string.title_contact));
+
+        // 添加对底部按钮点击的监听
         mNavigation.setOnNavigationItemSelectedListener(this);
 
-        Glide.with(this).load(R.drawable.bg_src_morning).centerCrop().into(new ViewTarget<View, GlideDrawable>(mLayAppbar) {
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View, GlideDrawable>(mLayAppbar) {
             @Override
             public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
                 this.view.setBackground(resource.getCurrent());
@@ -62,6 +87,11 @@ public class MainActivity extends Activity implements BottomNavigationView.OnNav
     @Override
     protected void initData() {
         super.initData();
+
+        // 从底部导航中接管menu，然后进行手动的触发第一次点击
+        Menu menu = mNavigation.getMenu();
+        // 触发首次选中Home
+        menu.performIdentifierAction(R.id.action_home, 0);
     }
 
 
@@ -75,10 +105,57 @@ public class MainActivity extends Activity implements BottomNavigationView.OnNav
 
     }
 
+    /**
+     * 当我们的底部导航被点击的时候触发
+     *
+     * @param item MenuItem
+     * @return True 代表我们能够处理这个点击
+     */
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        mTitle.setText(item.getTitle());
 
-        return true;
+        // 转接事件流到工具类中
+        return navHelper.performClickMenu(item.getItemId());
+    }
+
+    /**
+     * NavHelper 处理后回调的方法
+     *
+     * @param newTab
+     * @param oldTab
+     */
+    @Override
+    public void onTabChanged(NavHelper.Tab<Integer> newTab, NavHelper.Tab<Integer> oldTab) {
+        // 从额外字段中取出我们的Title资源ID
+        mTitle.setText(newTab.extra);
+
+        // 对浮动按钮进行隐藏与显示的动画
+        float transY = 0;
+        float rotation = 0;
+
+        if (Objects.equals(newTab.extra, R.string.title_home)) {
+//            transY = 100;
+            // 主界面时隐藏
+            transY = Ui.dipToPx(getResources(), 76);
+        } else {
+
+            // transY 默认为0 显示
+            if (Objects.equals(newTab.extra, R.string.title_group)) {
+                mAction.setImageResource(R.drawable.ic_group_add);
+                rotation = -360;
+            } else {
+                mAction.setImageResource(R.drawable.ic_contact_add);
+                rotation = 360;
+            }
+        }
+
+        // 开始动画
+        // 旋转，Y轴位移，弹性差值器，时间
+        mAction.animate()
+                .rotation(rotation)
+                .translationY(transY)
+                .setInterpolator(new AnticipateOvershootInterpolator(1))
+                .setDuration(480)
+                .start();
     }
 }
---
+++ App/iTalker/app/src/main/java/net/qiujuer/italker/push/frags/main/GroupFragment.java
@@ -0,0 +1,28 @@
+package net.qiujuer.italker.push.frags.main;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.qiujuer.italker.common.app.Fragment;
+import net.qiujuer.italker.push.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GroupFragment extends Fragment {
+
+
+    public GroupFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.fragment_group;
+    }
+
+}
